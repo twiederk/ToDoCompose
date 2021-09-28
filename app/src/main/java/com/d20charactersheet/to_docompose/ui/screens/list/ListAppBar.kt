ba0fa30c8b71a65aa3eebb6832f2ff8e832b69ad
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-09-28 06:36:54 +0200

    Start implementation of DefaultListAppBar

---
+++ app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/list/ListAppBar.kt
@@ -0,0 +1,34 @@
+package com.d20charactersheet.to_docompose.ui.screens.list
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.d20charactersheet.to_docompose.ui.theme.topAppBarContentColor
+
+@Composable
+fun ListAppBar() {
+    DefaultListAppBar()
+}
+
+
+@Composable
+fun DefaultListAppBar() {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Tasks",
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.primary
+    )
+}
+
+
+@Preview
+@Composable
+private fun DefaultLstAppBarPreview() {
+    DefaultListAppBar()
+}
