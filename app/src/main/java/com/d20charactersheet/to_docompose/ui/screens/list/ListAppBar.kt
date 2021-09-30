06bbdd2548b4ab9aa5c73459c71dcfce7dd85092
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-09-30 05:21:13 +0200

    Added SearchAction

--- app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/list/ListAppBar.kt
@@ -1,21 +1,27 @@
 package com.d20charactersheet.to_docompose.ui.screens.list
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.d20charactersheet.to_docompose.R
 import com.d20charactersheet.to_docompose.ui.theme.topAppBarBackgroundColor
 import com.d20charactersheet.to_docompose.ui.theme.topAppBarContentColor
 
 @Composable
 fun ListAppBar() {
-    DefaultListAppBar()
+    DefaultListAppBar(
+        onSearchClicked = {}
+    )
 }
 
 
 @Composable
-fun DefaultListAppBar() {
+fun DefaultListAppBar(
+    onSearchClicked: () -> Unit
+) {
     TopAppBar(
         title = {
             Text(
@@ -23,13 +29,41 @@ fun DefaultListAppBar() {
                 color = MaterialTheme.colors.topAppBarContentColor
             )
         },
+        actions = {
+            ListAppBarActions(onSearchClicked = onSearchClicked)
+        },
         backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor
     )
+}
+
+
+@Composable
+fun ListAppBarActions(
+    onSearchClicked: () -> Unit
+) {
+    SearchAction(onSearchClicked = onSearchClicked)
+}
+
+@Composable
+fun SearchAction(
+    onSearchClicked: () -> Unit
+) {
+    IconButton(onClick = { onSearchClicked() }
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Search,
+            contentDescription = stringResource(id = R.string.search_action),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+
 }
 
 
 @Preview
 @Composable
 private fun DefaultLstAppBarPreview() {
-    DefaultListAppBar()
+    DefaultListAppBar(
+        onSearchClicked = {}
+    )
 }
