fb69d7fee6e43b18b21d764453f5a811f9a8d749
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-09-30 04:58:08 +0200

    Improved ListFab.

--- app/src/main/java/com/d20charactersheet/to_docompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/d20charactersheet/to_docompose/navigation/destinations/ListComposable.kt
@@ -10,7 +10,7 @@ import com.d20charactersheet.to_docompose.util.Constants.LIST_ARGUMENT_KEY
 import com.d20charactersheet.to_docompose.util.Constants.LIST_SCREEN
 
 fun NavGraphBuilder.listComposable(
-    navigateToTaskScreen: (Int) -> Unit
+    navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
     composable(
         route = LIST_SCREEN,
