c1786344b76c2923e3f7a7425bc2c12aab51036f
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-10-02 06:04:29 +0200

    Load all task from database and display them

--- app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/list/ListContent.kt
@@ -2,6 +2,8 @@ package com.d20charactersheet.to_docompose.ui.screens.list
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,9 +19,26 @@ import com.d20charactersheet.to_docompose.data.models.Priority
 import com.d20charactersheet.to_docompose.data.models.ToDoTask
 import com.d20charactersheet.to_docompose.ui.theme.*
 
+@ExperimentalMaterialApi
 @Composable
-fun ListContent() {
+fun ListContent(
+    tasks: List<ToDoTask>,
+    navigateToTaskScreen: (taskId: Int) -> Unit
+) {
+    LazyColumn {
+        items(
+            items = tasks,
+            key = { task ->
+                task.id
+            }
+        ) { task ->
+            TaskItem(
+                toDoTask = task,
+                navigateToTaskScreen = navigateToTaskScreen
+            )
 
+        }
+    }
 }
 
 
