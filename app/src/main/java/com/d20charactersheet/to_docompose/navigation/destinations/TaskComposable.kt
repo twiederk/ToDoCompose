85cccc5d67746c9564eaed1477395a4add9d67f5
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-10-04 06:16:47 +0200

    Select task from database.

--- app/src/main/java/com/d20charactersheet/to_docompose/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/d20charactersheet/to_docompose/navigation/destinations/TaskComposable.kt
@@ -1,16 +1,20 @@
 package com.d20charactersheet.to_docompose.navigation.destinations
 
 //import androidx.navigation.compose.navArgument
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.d20charactersheet.to_docompose.ui.screens.task.TaskScreen
+import com.d20charactersheet.to_docompose.ui.viewmodels.SharedViewModel
 import com.d20charactersheet.to_docompose.util.Action
 import com.d20charactersheet.to_docompose.util.Constants.TASK_ARGUMENT_KEY
 import com.d20charactersheet.to_docompose.util.Constants.TASK_SCREEN
 
 fun NavGraphBuilder.taskComposable(
+    sharedViewModel: SharedViewModel,
     navigateToListScreen: (Action) -> Unit
 ) {
     composable(
@@ -20,6 +24,12 @@ fun NavGraphBuilder.taskComposable(
         })
     ) { navBackStackEntry ->
         val taskId = navBackStackEntry.arguments!!.getInt(TASK_ARGUMENT_KEY)
-        TaskScreen(navigateToListScreen = navigateToListScreen)
+        sharedViewModel.getSelectedTask(taskId = taskId)
+        val selectedTask by sharedViewModel.selectedTask.collectAsState()
+
+        TaskScreen(
+            selectedTask = selectedTask,
+            navigateToListScreen = navigateToListScreen
+        )
     }
 }
--- app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/task/TaskScreen.kt
@@ -2,16 +2,21 @@ package com.d20charactersheet.to_docompose.ui.screens.task
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import com.d20charactersheet.to_docompose.data.models.ToDoTask
 import com.d20charactersheet.to_docompose.util.Action
 
 @Composable
 fun TaskScreen(
+    selectedTask: ToDoTask?,
     navigateToListScreen: (Action) -> Unit
 ) {
 
     Scaffold(
         topBar = {
-            TaskAppBar(navigateToListScreen = navigateToListScreen)
+            TaskAppBar(
+                selectedTask = selectedTask,
+                navigateToListScreen = navigateToListScreen
+            )
         },
         content = {}
     )
