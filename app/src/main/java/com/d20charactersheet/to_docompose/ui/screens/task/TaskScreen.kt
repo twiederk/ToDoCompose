aebaa6718bfc3b19a0f8e079eab6f716904bf11a
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-10-04 05:38:09 +0200

    Added NewTaskAppBar

---
+++ app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/task/TaskScreen.kt
@@ -0,0 +1,15 @@
+package com.d20charactersheet.to_docompose.ui.screens.task
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskScreen() {
+
+    Scaffold(
+        topBar = {
+
+        },
+        content = {}
+    )
+}
