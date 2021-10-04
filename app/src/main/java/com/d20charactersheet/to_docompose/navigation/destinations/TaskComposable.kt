cda8bcb50ac30c4c24fe3ad34b3de27ef8bf2fc7
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-10-04 05:58:23 +0200

    Added ExistingTaskAppBar

--- app/src/main/java/com/d20charactersheet/to_docompose/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/d20charactersheet/to_docompose/navigation/destinations/TaskComposable.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.d20charactersheet.to_docompose.ui.screens.task.TaskScreen
 import com.d20charactersheet.to_docompose.util.Action
 import com.d20charactersheet.to_docompose.util.Constants.TASK_ARGUMENT_KEY
 import com.d20charactersheet.to_docompose.util.Constants.TASK_SCREEN
@@ -19,5 +20,6 @@ fun NavGraphBuilder.taskComposable(
         })
     ) { navBackStackEntry ->
         val taskId = navBackStackEntry.arguments!!.getInt(TASK_ARGUMENT_KEY)
+        TaskScreen(navigateToListScreen = navigateToListScreen)
     }
 }
--- app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/task/TaskAppBar.kt
+++ app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/task/TaskAppBar.kt
@@ -4,10 +4,15 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.d20charactersheet.to_docompose.R
+import com.d20charactersheet.to_docompose.data.models.Priority
+import com.d20charactersheet.to_docompose.data.models.ToDoTask
 import com.d20charactersheet.to_docompose.ui.theme.topAppBarBackgroundColor
 import com.d20charactersheet.to_docompose.ui.theme.topAppBarContentColor
 import com.d20charactersheet.to_docompose.util.Action
@@ -70,10 +75,93 @@ fun AddAction(
 }
 
 
+@Composable
+fun ExistingTaskAppBar(
+    selectedTask: ToDoTask,
+    navigateToListScreen: (Action) -> Unit
+) {
+    TopAppBar(
+        navigationIcon = {
+            CloseAction(onCloseClicked = navigateToListScreen)
+        },
+        title = {
+            Text(
+                text = selectedTask.title,
+                color = MaterialTheme.colors.topAppBarContentColor,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            DeleteAction(onDeleteClicked = navigateToListScreen)
+            UpdateAction(onUpdateClicked = navigateToListScreen)
+        }
+    )
+}
+
+
+@Composable
+fun CloseAction(
+    onCloseClicked: (Action) -> Unit
+) {
+    IconButton(onClick = { onCloseClicked(Action.NO_ACTION) }) {
+        Icon(
+            imageVector = Icons.Filled.Close,
+            contentDescription = stringResource(id = R.string.close_icon),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+}
+
+
+@Composable
+fun DeleteAction(
+    onDeleteClicked: (Action) -> Unit
+) {
+    IconButton(onClick = { onDeleteClicked(Action.DELETE) }) {
+        Icon(
+            imageVector = Icons.Filled.Delete,
+            contentDescription = stringResource(id = R.string.delete_icon),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+}
+
+
+@Composable
+fun UpdateAction(
+    onUpdateClicked: (Action) -> Unit
+) {
+    IconButton(onClick = { onUpdateClicked(Action.UPDATE) }) {
+        Icon(
+            imageVector = Icons.Filled.Check,
+            contentDescription = stringResource(id = R.string.update_icon),
+            tint = MaterialTheme.colors.topAppBarContentColor
+        )
+    }
+}
+
+
 @Preview
 @Composable
 fun NewTaskAppBarPreview() {
     NewTaskAppBar(
+        navigateToListScreen = {}
+    )
+}
+
+
+@Preview
+@Composable
+fun ExistingTaskAppBarPreview() {
+    ExistingTaskAppBar(
+        selectedTask = ToDoTask(
+            id = 0,
+            title = "Stevdaz-San",
+            description = "Some random text",
+            priority = Priority.LOW
+        ),
         navigateToListScreen = {}
     )
 }
