d321298c649f606652dfa91c0528b3a6c8687557
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-10-08 05:53:46 +0200

    Created DataStoreRepository.

--- app/src/main/java/com/d20charactersheet/to_docompose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/d20charactersheet/to_docompose/ui/viewmodels/SharedViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.d20charactersheet.to_docompose.data.models.Priority
 import com.d20charactersheet.to_docompose.data.models.ToDoTask
+import com.d20charactersheet.to_docompose.data.repositories.DataStoreRepository
 import com.d20charactersheet.to_docompose.data.repositories.ToDoRepository
 import com.d20charactersheet.to_docompose.util.Action
 import com.d20charactersheet.to_docompose.util.Constants.MAX_TITLE_LENGTH
@@ -21,7 +22,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SharedViewModel @Inject constructor(
-    private val repository: ToDoRepository
+    private val repository: ToDoRepository,
+    private val dataStoreRepository: DataStoreRepository
 ) : ViewModel() {
 
     val action: MutableState<Action> = mutableStateOf(Action.NO_ACTION)
--- app/src/main/java/com/d20charactersheet/to_docompose/util/Constants.kt
+++ app/src/main/java/com/d20charactersheet/to_docompose/util/Constants.kt
@@ -7,6 +7,9 @@ object Constants {
     const val LIST_SCREEN = "list/{action}"
     const val TASK_SCREEN = "task/{taskId}"
 
+    const val PREFERENCE_NAME = "todo_preferences"
+    const val PREFERENCE_KEY = "sort_state"
+
     const val LIST_ARGUMENT_KEY = "action"
     const val TASK_ARGUMENT_KEY = "taskId"
 
