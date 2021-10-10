3b7857d8402f893bbe42f9b27f1c01b3d1207e1a
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2021-10-10 10:51:44 +0200

    Bug fix: Snack bar dismiss immediately

--- app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/d20charactersheet/to_docompose/ui/screens/list/ListScreen.kt
@@ -65,6 +65,7 @@ fun ListScreen(
                 onSwipeToDelete = { action, task ->
                     sharedViewModel.action.value = action
                     sharedViewModel.updateTaskFields(selectedTask = task)
+                    scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
                 },
                 navigateToTaskScreen = navigateToTaskScreen
             )
